f05bd8e87e784580d7b323de5a8f315255898ae4
os-sulX1 <sultanxdpx@gmail.com>
2023-12-06 15:59:59 +0300

    Infinite Scroll in Next js 14

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LoadMore from "../components/LoadMore";
 import { fetchAnime } from "./action";
 
 async function Home() {
-  const data = await fetchAnime(2)
+  const data = await fetchAnime(1)
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold"> Anime</h2>
